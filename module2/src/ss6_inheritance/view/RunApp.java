10858fbc363777c1ffff08d6da3aad757407342f
chanhtv235 <chanhtv235@gmail.com>
2026-02-03 20:11:47 +0700

    update mvc moi nhat

--- module2/src/ss6_inheritance/view/RunApp.java
+++ module2/src/ss6_inheritance/view/RunApp.java
@@ -1,11 +1,11 @@
 package ss6_inheritance.view;
 
+import ss6_inheritance.controller.ControllerCodegym;
 import ss6_inheritance.controller.StudentController;
 
 public class RunApp {
     public static void main(String[] args) {
-
-        StudentController studentController = new StudentController();
-        studentController.displayMenu();
+        ControllerCodegym controllerCodegym = new ControllerCodegym();
+        controllerCodegym.displayMenu();
     }
 }
